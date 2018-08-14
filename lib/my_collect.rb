59de9438659e413fde57c62f60e9947413e7ce30
Mendy <mendybeltcohen@gmail.com>
2018-08-14 19:08:44 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
-  i = 0
-  while i < collection.length
-  yield collection[i]
-  i+=1
+  arr = []
+  while arr.length < collection.length
+  yield collection[arr]
+  
  end
- i
+ arr
 end
 
